3b30ab6981f597f686f1a51a38d4475779169f40
4015c2 <4015c2@guineo.ece.uprm.edu>
2017-02-08 12:43:33 -0400

    Changes in title, size and location.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Carlos");
+		JFrame myFrame = new JFrame("Carlos") ;
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 400);
 		myFrame.setSize(200, 200);
